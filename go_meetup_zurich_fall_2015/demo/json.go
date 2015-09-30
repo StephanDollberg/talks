6090262cb248819bd0efad64c248a50ef159aa21
Stephan Dollberg <stephan.dollberg@gmail.com>
2015-09-30 16:26:34 +0200

    more work

--- go_meetup_zurich_fall_2015/demo/json.go
+++ go_meetup_zurich_fall_2015/demo/json.go
@@ -6,6 +6,21 @@ import (
 	"log"
 )
 
+type Msg1 struct {
+	msg string
+	id  int64
+}
+
+type Msg2 struct {
+	Msg string
+	Id  int64
+}
+
+type Msg3 struct {
+	Msg string `json:"msg"`
+	Id  int64  `json:"id"`
+}
+
 func main() {
 
 	{
@@ -30,5 +45,44 @@ func main() {
 		}
 
 		fmt.Println(decoded["msg"].(string), int(decoded["id"].(float64)))
+	}
+
+	{
+		raw_data := []byte(`{"msg": "Hello Go!", "id": 12345}`)
+		decoded := Msg1{}
+		err := json.Unmarshal(raw_data, &decoded)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Println(decoded.msg, decoded.id)
+	}
+
+	{
+		raw_data := []byte(`{"msg": "Hello Go!", "id": 12345}`)
+		decoded := Msg2{}
+		err := json.Unmarshal(raw_data, &decoded)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Println(decoded.Msg, decoded.Id)
+
+		encoded, _ := json.Marshal(&decoded)
+		fmt.Println(string(encoded))
+	}
+
+	{
+		raw_data := []byte(`{"msg": "Hello Go!", "id": 12345}`)
+		decoded := Msg3{}
+		err := json.Unmarshal(raw_data, &decoded)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Println(decoded.Msg, decoded.Id)
 	}
 }
